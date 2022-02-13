549577975ed275e410b332374ac5046383eea4b9
Magsy-coder <98486064+Magsy-coder@users.noreply.github.com>
2022-02-13 21:42:16 +0100

    api forecast function

--- src/index.js
+++ src/index.js
@@ -40,7 +40,8 @@ function formatDate(datestamp) {
   return `${day} ${hours}.${minutes}`;
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
@@ -66,6 +67,13 @@ function showForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "01c069a1dd0ad5193fff058ba13e03c8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showForecast);
+}
+
 function showTemperature(response) {
   let currentLocation = document.querySelector("#current-location");
 
@@ -87,6 +95,8 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function showWeather(yourLocation) {
@@ -133,8 +143,6 @@ function showCelsiusTemp(event) {
 }
 
 celsiusTemperature = null;
-
-showForecast();
 
 let currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", findGeolocation);
